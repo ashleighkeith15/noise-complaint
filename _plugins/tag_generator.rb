036fed77b056f4e2b1548229906704fce6160cc1
ashleighkeith15 <133888224+ashleighkeith15@users.noreply.github.com>
2025-02-01 08:59:53 -0600

    tag auto files creation

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,38 @@
+module Jekyll
+    class TagPageGenerator < Generator
+      safe true
+  
+      def generate(site)
+        tags = []
+        
+        # Collect all tags from notes
+        site.collections['notes'].docs.each do |note|
+          if note.data['tags']
+            tags.concat(note.data['tags'])
+          end
+        end
+  
+        # Remove duplicates
+        tags = tags.uniq
+  
+        # Create a page for each tag
+        tags.each do |tag|
+          site.pages << TagPage.new(site, site.source, 'tags', tag)
+        end
+      end
+    end
+  
+    class TagPage < Page
+      def initialize(site, base, dir, tag)
+        @site = site
+        @base = base
+        @dir = dir
+        @name = "#{tag}.html"
+  
+        self.process(@name)
+        self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+        self.data['title'] = "Tagged: #{tag}"
+        self.data['tag'] = tag
+      end
+    end
+  end
